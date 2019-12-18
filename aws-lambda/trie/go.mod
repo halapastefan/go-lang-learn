4d32b0411e4f155dd7702d79577fc2a7038e6b37
halapastefan <halapastefan@outlook.com>
2019-12-18 16:31:49 +0100

    trie lambda

---
+++ aws-lambda/trie/go.mod
@@ -0,0 +1,10 @@
+module github.com/halapastefan/aws/trie
+
+go 1.13
+
+require (
+	github.com/derekparker/trie v1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/aws/aws-lambda-go v1.13.3
+	github.com/aws/aws-lambda-go/events v1.13.3
+)
---
+++ aws-lambda/trie/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/derekparker/trie"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"os"
+)
+var timeFormat = "2006-01-02T15:04:05.00Z"
+var t *trie.Trie
+
+func init()  {
+	csvFile, err := os.Open("callingCodes.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	CreateTrie(csvFile)
+}
+type nodeValue struct {
+	value []entry
+}
+type entry struct {
+	price string
+	initial int
+	increment int
+	startDate time.Time
+}
+
+func find(req events.APIGatewayProxyRequest)(events.APIGatewayProxyResponse, error) {
+	calling := req.QueryStringParameters["calling"]
+	date := req.QueryStringParameters["date"]
+	findPrice(t, calling, date)
+
+	return events.APIGatewayProxyResponse{StatusCode:200}, nil
+}
+func main() {
+
+	lambda.Start(find)
+}
+func findPrice(t *trie.Trie, calling string, date string) entry {
+	r, _ := findPrefix(calling, t)
+	d, _ := time.Parse(timeFormat, date)
+	return matchDate(*r, d)
+}
+
+func findPrefix(nm string, trie *trie.Trie) (*nodeValue, bool) {
+
+	i := 0
+	for range nm {
+		search := nm[0:len(nm)-i]
+		node, found := trie.Find(search)
+		if found {
+			retVal := node.Meta().(nodeValue)
+			return &retVal, true
+		}
+		i++
+	}
+
+	return nil, false
+}
+func matchDate(value nodeValue, date time.Time) entry {
+	for _, v := range value.value{
+		start := v.startDate
+		end := start.AddDate(0, 1, 0)
+		if date.After(start) && date.Before(end) {
+			return v
+		}
+	}
+	sort.Slice(value.value, func(i, j int) bool {
+		return value.value[j].startDate.Before(value.value[i].startDate)
+	})
+
+	return value.value[0]
+}
+
+//From csv line create entry that will go to the trie
+func createEntry(line []string) *entry  {
+	price := line[3]
+	initial, _ := strconv.Atoi(line[4])
+	increment, _ := strconv.Atoi(line[5])
+	start, dErr := time.Parse(timeFormat, line[6])
+	if dErr != nil {
+		fmt.Println(dErr)
+	}
+
+	return &entry{
+		price:     price,
+		initial:   initial,
+		increment: increment,
+		startDate: start,
+	}
+}
+
+//Meta data of node will be appended if node already exists
+func appendNode(t *trie.Trie, line []string){
+	node, found := t.Find(line[0])
+	if found {
+		v := node.Meta().(nodeValue)
+		v.value = append(v.value, *createEntry(line))
+		t.Add(line[0], v)
+	} else {
+		en := make([]entry, 0)
+		en = append(en, *createEntry(line))
+		nv := nodeValue{
+			value:en,
+		}
+		t.Add(line[0], nv)
+	}
+}
+
+func CreateTrie(file *os.File) *trie.Trie {
+	t := trie.New()
+
+	reader := csv.NewReader(file)
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		if line[0]!="prefix" {
+			appendNode(t, line)
+		}
+
+	}
+
+	return t
+}
