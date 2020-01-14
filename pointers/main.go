9fde1fa5a69f0fb0fe5d5a65a283cfd6a813da27
halapastefan <halapastefan@outlook.com>
2020-01-14 11:28:10 +0100

    Pointers vs copy

--- pointers/main.go
+++ pointers/main.go
@@ -29,3 +29,25 @@ func main() {
 
 	fmt.Println(runtime.NumCPU())
 }
+
+type S struct {
+	a, b, c int64
+	d, e, f string
+	g, h, i float64
+}
+
+func byCopy() S {
+	return S{
+		a: 1, b: 1, c: 1,
+		e: "foo", f: "foo",
+		g: 1.0, h: 1.0, i: 1.0,
+	}
+}
+
+func byPointer() *S {
+	return &S{
+		a: 1, b: 1, c: 1,
+		e: "foo", f: "foo",
+		g: 1.0, h: 1.0, i: 1.0,
+	}
+}
