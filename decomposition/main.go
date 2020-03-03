e76eb9b23518b40faab266dd6451210afb8a0db8
Stefan Halapa <halapastefan@outlook.com>
2020-03-03 21:17:29 +0100

    interfaces

---
+++ decomposition/main.go
@@ -0,0 +1,102 @@
+package main
+
+
+func main() {
+
+	// Declare a variable of type data.
+	d := data{
+		name: "Bill",
+	}
+
+	fmt.Println("Proper Calls to Methods:")
+
+	// How we actually call methods in Go.
+	d.displayName()
+	d.setAge(45)
+
+	fmt.Println("\nWhat the Compiler is Doing:")
+
+	// This is what Go is doing underneath.
+	// DONT DO THIS!!!
+	data.displayName(d)
+	(*data).setAge(&d, 45)
+
+	// =========================================================================
+
+	fmt.Println("\nCall Value Receiver Methods with Variable:")
+
+	// Declare a function variable for the method bound to the d variable.
+	// The function variable will get its own copy of d because the method
+	// is using a value receiver.
+	f1 := d.displayName
+
+	// Call the method via the variable.
+	f1()
+
+	// Change the value of d.
+	d.name = "Joan"
+
+	// Call the method via the variable. We don't see the change.
+	f1()
+
+	// =================================================================fmt.Println("Proper Calls to Methods:")
+
+	// How we actually call methods in Go.
+	d.displayName()
+	d.setAge(45)
+
+	fmt.Println("\nWhat the Compiler is Doing:")
+
+	// This is what Go is doing underneath.
+	data.displayName(d)
+	(*data).setAge(&d, 45)
+
+	// =========================================================================
+
+	fmt.Println("\nCall Value Receiver Methods with Variable:")
+
+	// Declare a function variable for the method bound to the d variable.
+	// The function variable will get its own copy of d because the method
+	// is using a value receiver.
+	f1 := d.displayName
+
+	// Call the method via the variable.
+	f1()
+
+	// Change the value of d.
+	d.name = "Joan"
+
+	// Call the method via the variable. We don't see the change.
+	f1()
+
+	// =========================================================================
+
+	fmt.Println("\nCall Pointer Receiver Method with Variable:")
+
+	// Declare a function variable for the method bound to the d variable.
+	// The function variable will get the address of d because the method
+	// is using a pointer receiver.
+	f2 := d.setAge
+
+	// Call the method via the variable.
+	f2(45)
+
+	// Change the value of d.
+	d.name = "Sammy"
+
+	// Call the method via the variable. We see the change.
+	f2(45)e.
+	// The function variable will get the address of d because the method
+	// is using a pointer receiver.
+	f2 := d.setAge
+
+	// Call the method via the variable.
+	f2(45)
+
+	// Change the value of d.
+	d.name = "Sammy"
+
+	// Call the method via the variable. We see the change.
+	f2(45)
+
+}
